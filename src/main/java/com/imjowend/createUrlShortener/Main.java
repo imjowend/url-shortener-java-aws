c1832595984586e4061014a93fa635771dfeda35
imjowend <joaquinvcestudio@gmail.com>
2024-11-26 19:50:28 -0500

    TODO

--- src/main/java/com/imjowend/createUrlShortener/Main.java
+++ src/main/java/com/imjowend/createUrlShortener/Main.java
@@ -28,7 +28,7 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
         String shortUrlCode = UUID.randomUUID().toString().substring(0 , 8);
 
-
+        // TODO: Clase 2 de Curso
 
         Map<String, String> response = new HashMap<>();
         response.put("code", shortUrlCode);
